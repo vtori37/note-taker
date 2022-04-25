f32f9bebfc258682b2c875c9f2c624905f705fe8
Tori Acer <vrice360@gmail.com>
2022-04-24 19:33:44 -0500

    first commit to heroku

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,23 +1,22 @@
 const router = require('express').Router();
 const fs = require('fs');
 const uniqid = require('uniqid');
-const 
-path = require('path');
+const path = require('path');
+const notes = require('../db/db.json');
 //to retrieve notes
 router.get('/notes', (req, res) => {
-  let result = notes;
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
+  fs.readFile(notes, (err,data) => {
+  if(err) res.sendStatus(404);
+  console.log(data);
+  res.json(JSON.parse(data));
+  })
 });
 
 
 // to post notes
 router.post('/notes', (req, res) => {
 req.body.id = notes.length.toString();
-
+fs.writeFile
 });
 
 
