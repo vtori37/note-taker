31ba7e5fde76df67da7d730d88b4f22c51226ae7
Tori Acer <vrice360@gmail.com>
2022-04-24 23:07:29 -0500

    api-routes new issue

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,10 +6,11 @@ const notes = require('../db/db.json');
 
 //to retrieve notes
 router.get('/notes', (req, res) => {
-  fs.readFile(notes, (err,data) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'), (err,data) => {
   if(err) res.sendStatus(404);
   console.log(data);
   res.json(JSON.parse(data));
+  console.log(process.cwd());
   })
 });
 
@@ -18,7 +19,7 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
   const newNotes = req.body
   newNotes.id = uuidv4();
-  let readData = JSON.parse(fs.readFileSync('../db/db.json', "utf8"));
+  let readData = JSON.parse(path.join(__dirname,('../db/db.json', "utf8")));
 
   readData.push(newNotes);
 
@@ -35,7 +36,7 @@ res.json(readData);
 // to delete notes 
 router.delete('/notes/:id', (req, res) => {
   let savedNotesId = req.params.id;
-  let readData = JSON.parse(fs.readFileSync('../db/db.json', "utf8"));
+  let readData = JSON.parse(path.join(__dirname('../db/db.json', "utf8")));
 
   let findData = readData.filter(note => note.id.length !== note)
 });
